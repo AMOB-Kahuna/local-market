93a4da2302c2bb25fbf449ba67407a69025dbc4c
AMOB-Kahuna <balloabdulmujeeb42@.com>
2026-05-25 08:01:31 +0100

    Initiate react-router, create components and pages folder, create site layout, and header section in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router"
+import Layout from "./components/Layout"
+import Homepage from "./pages/Homepage"
 
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+   <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Homepage />} />
+    </Route>
+   </Routes>
   )
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+import { Outlet } from 'react-router'
+
+const Layout = () => {
+
+
+  return (
+    <>
+      <header className='px-3 py-4 border-b border-b-[#F0A500]'>
+        <h1 className='text-[#5C3317] font-[Abril_Fatface] font-bold text-2xl'>LocalMarket</h1>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default Layout
