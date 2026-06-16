8f2acba8b631a590d077720c9c66afa18b6e1a6b
AMOB <balloabdulmujeeb42@gmail.com>
2026-06-16 06:41:22 +0100

    Fix formatting and indentation in Layout component

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -61,7 +61,7 @@ const Layout = () => {
           className={`
             absolute top-16 right-3 z-40 w-50 flex-col gap-4 rounded-xl bg-[#FFFDF5] p-5 shadow-md
             ${showNav ? 'flex' : 'hidden'}
-            sm:static sm:flex sm:flex-row sm:gap-5 sm:shadow-none text-xl
+            sm:flex sm:flex-row sm:gap-5 sm:shadow-none text-xl
           `}
         >
           <button onClick={() => setShowNav(false)}>
